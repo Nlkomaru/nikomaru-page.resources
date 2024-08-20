91e786ff7d09c19415c55fddfbe6497a065b6fc5
Nlkomaru <nikomaru@nikomaru.dev>
2024-08-20 12:38:42 +0900

    Init commit

---
+++ page/src/app/docs/[id]/page.tsx
@@ -0,0 +1,50 @@
+import ReactMarkdown from 'react-markdown';
+import Image from "next/image";
+import remarkGfm from 'remark-gfm';
+import remarkBreaks from 'remark-breaks';
+import {ReactElement, ReactNode} from "react";
+import {css} from "@/styled-system/css";
+
+export const runtime = 'edge';
+
+interface ImgProps {
+    src?: string;  // src is now optional
+    alt?: string;
+    children?: ReactNode;
+}
+
+const components = {
+    img: ({src, alt, children}: ImgProps): ReactElement =>
+        <Image className={css({display: "block", padding: "20px"})} src={src!!} alt={alt!!} width={1920}
+               height={1080}>{children}</Image>,
+};
+
+export default async function RemoteMdxPage({params}: { params: { id: string } }) {
+    // MDX text - can be from a local file, database, CMS, fetch, anywhere...
+    const id = params.id
+    const res = await fetch("https://resources.s3.nikomaru.dev/" + id + "/docs.md")
+    const markdown = await res.text()
+
+
+    const urlTransform = (url: string) => {
+        if (!url.startsWith("./static")) {
+            return url
+        }
+        return `https://resources.s3.nikomaru.dev/${id}/${url.replace("./", "")}`
+    }
+
+    return (<div className={css({backgroundColor: "#d1f9d4", padding: "100px"})}>
+        <div className={css({
+            width: "800px",
+            margin: "auto",
+            padding: "50px",
+            fontSize: "l",
+            backgroundColor: "white",
+        })}>
+            <ReactMarkdown className='markdown'
+                           urlTransform={urlTransform}
+                           remarkPlugins={[remarkGfm, remarkBreaks]}
+                           components={components}>{markdown}</ReactMarkdown>
+        </div>
+    </div>)
+}
---
+++ page/src/app/page.tsx
@@ -0,0 +1,101 @@
+import {css} from '@/styled-system/css';
+import React from "react";
+import SlideCard from "~/components/SlideCard";
+
+export const runtime = "edge";
+
+export default async function Home() {
+    let bucket = process.env.BUCKET
+
+    if (!bucket) {
+        throw new Error("BUCKET is not defined in the environment");
+    }
+    let list = (await bucket.list()).objects.map((o) => o.key.split("/")[0])
+    let data = Array.from(new Set(list))
+
+    let slideList: SlideGroup[] = [
+        {
+            "Group": "Container",
+            "list": [
+                {slide: "container-1", title: "コンテナについて"},
+                {slide: "docker-1", title: "Dockerについて"},
+                {slide: "kubernetes-1", title: "Kubernetesについて"},
+            ]
+        }
+    ]
+
+    slideList.forEach((group) => {
+        group.list.map((slide) => {
+            data = data.filter(n => n !== slide.slide);
+        })
+    })
+
+    if (data.length !== 0) {
+        slideList.push({
+            "Group": "その他",
+            "list": data.map((slide) => {
+                return {slide: slide, title: slide}
+            })
+        })
+    }
+
+    return (
+        <>
+            <div className={css({fontSize: "xl", fontWeight: 'bold', paddingBottom: "30px"})}>Slide List</div>
+
+            {slideList.map((slide) => (
+                <>
+                    <h2 className={css({fontSize: "xl", fontWeight: 'bold', paddingBottom: "10px"})}>{slide.Group}</h2>
+                    <ul className={css({display: "flex"})}>
+                        {slide.list
+                            .map((slideName) => getSlideInfo(slideName.slide, slideName.title))
+                            .map((slideInfo) => (
+                                <li key={slideInfo.slideTitle}>
+                                    <SlideCard slideInfo={slideInfo}/>
+                                </li>
+                            ))}
+                    </ul>
+                </>
+            ))
+            }
+        </>
+    )
+}
+
+function getSlideInfo(slide: string, title: string): SlideInfo {
+    return {
+        slideTitle: title,
+        slideName: slide,
+        slideUrl: `https://slide.moripa.nikomaru.dev/${slide}/index.html`,
+        pdfUrl: `https://slide.moripa.nikomaru.dev/${slide}/slidev-exported.pdf`,
+        imageUrl: `https://slide.moripa.nikomaru.dev/${slide}/picture/1.png`,
+    }
+}
+
+export type SlideInfo = {
+    slideTitle: string;
+    slideName: string;
+    slideUrl: string;
+    pdfUrl: string;
+    imageUrl: string;
+}
+
+export type SlideGroup = {
+    Group: string;
+    list: slide[];
+}
+
+export type slide = {
+    slide: string;
+    title: string;
+}
+
+
+
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            BUCKET: R2Bucket;
+        }
+    }
+}
